b38bbfe375fc024449df2b9262ebd1cc624e6a46
Ada <ajli@smith.edu>
2024-09-12 16:40:31 -0400

    Tried to fix the storeInformation function.

--- computer.py
+++ computer.py
@@ -12,7 +12,6 @@ class Computer:
     year_made: int
     price: int
     # How will you set up your constructor?
-    # I will use the self and set up the attributes to the respective type.
     # Remember: in python, all constructors have the same name (__init__)
     def __init__(self,description: str,
                     processor_type: str,
@@ -30,13 +29,19 @@ class Computer:
         self.price = price 
 
     # What methods will you need? 
-    def storeInformation(self,description:str, processor_type:str, hard_drive_capacity: int,
+    def storeInformation(self, description:str, processor_type:str, hard_drive_capacity: int,
                     memory: int,
                     operating_system: str,
                     year_made: int,
                     price: int):
-        description = self.description
+        description = self.description 
         processor_type = self.processor_type
+        hard_drive_capacity = self.hard_drive_capacity
+        memory = self.memory
+        operating_system = self.operating_system
+        year_made = self.year_made
+        price == self.price
+        print(description, processor_type,hard_drive_capacity,memory,operating_system,year_made,price)
 
 def main():
         my_computer = Computer(
