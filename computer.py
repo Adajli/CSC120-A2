a1d48fff784956207daec395d78d38d64815e8f6
Ada Li <ajli@smith.edu>
2024-09-12 10:24:30 -0400

    Update computer.py

--- computer.py
+++ computer.py
@@ -5,7 +5,7 @@ class Computer:
     # How will you set up your constructor?
     # I will use the self and set upt the attributes to the respective type.
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self,):
+    def __init__(self, my_description:):
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
