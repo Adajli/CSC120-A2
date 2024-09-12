4b83466b1ee22e757f76c9151987672dca4c8478
Ada Li <ajli@smith.edu>
2024-09-12 10:22:33 -0400

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,10 +1,11 @@
 class Computer:
 
     # What attributes will it need?
-
+    # The attributes it will need are the description, processor type, hard drive capacity, operating system, year made, and price.
     # How will you set up your constructor?
+    # I will use the self and set upt the attributes to the respective type.
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    def __init__(self,):
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
