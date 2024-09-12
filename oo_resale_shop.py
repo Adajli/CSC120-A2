35bda9801c878ee48bd225e76689ec8ea2ec97be
Ada <ajli@smith.edu>
2024-09-12 15:06:47 -0400

    Added traits to class ResaleShop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -5,7 +5,7 @@ class ResaleShop:
     new_price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    def __init__(self, item_id: int, new_price:int):
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
