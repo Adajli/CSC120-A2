761c626ff113ace7bffaf28a5eb6e3629ec09d75
Ada <ajli@smith.edu>
2024-09-12 10:44:31 -0400

    Added main in computer.py

--- computer.py
+++ computer.py
@@ -1,7 +1,13 @@
 class Computer:
 
     # What attributes will it need?
-    # The attributes it will need are the description, processor type, hard drive capacity, operating system, year made, and price.
+    description: str = "<NO DESC AVAILABLE>"
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
     # How will you set up your constructor?
     # I will use the self and set upt the attributes to the respective type.
     # Remember: in python, all constructors have the same name (__init__)
@@ -22,13 +28,13 @@ class Computer:
 
     # What methods will you need?
 
-    def main():
+def main():
         my_computer = Computer(
-            "Mac Pro (Late 2013)",
-            "3.5 GHc 6-Core Intel Xeon E5",
-            1024, 64,
-            "macOS Big Sur", 2013, 1500
+                "Mac Pro (Late 2013)",
+                "3.5 GHc 6-Core Intel Xeon E5",
+                1024, 64,
+                "macOS Big Sur", 2013, 1500
         )
         print(my_computer)
 
-    main()
+main()
